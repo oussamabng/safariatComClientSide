aa8352fc108eda344d5bfc4dce1fac1b6ecbe9ca
oussamabng <o.bengoudifa@esi-sba.dz>
2020-05-03 16:51:24 +0100

    add half of responsivity

--- frontend/src/components/ToursDetailNav/ToursDetailNav.jsx
+++ frontend/src/components/ToursDetailNav/ToursDetailNav.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Container, Menu } from "semantic-ui-react";
+import { Container, Menu, Accordion, Icon } from "semantic-ui-react";
+import { ReactComponent as Dots } from "../../assets/icons/dots.svg";
 
 //? import css
 import "./ToursDetailNav.css";
@@ -10,7 +11,12 @@ import TourGallery from "./TourGallery.jsx";
 
 const ToursDetailNav = () => {
   const [activeItem, setActiveItem] = useState("Overview");
+  const [isShow, setIsShow] = useState(false);
+
   const handleItemClick = (e, { name }) => setActiveItem(name);
+  const handleClick = () => {
+    setIsShow((prevState) => !prevState);
+  };
   return (
     <>
       <div className="detail_main_nav">
@@ -45,6 +51,76 @@ const ToursDetailNav = () => {
             </Menu>
           </div>
         </Container>
+        <div className="detail_main_nav_mobile">
+          <Accordion>
+            <Accordion.Title index={0} className={isShow ? "hidde" : ""}>
+              <div className="ouf">
+                <Menu secondary className="_tour_detail_nav">
+                  <Menu.Item
+                    name="Overview"
+                    active={activeItem === "Overview"}
+                    onClick={handleItemClick}
+                  />
+                  <Menu.Item
+                    name="Itinerary"
+                    active={activeItem === "Itinerary"}
+                    onClick={handleItemClick}
+                  />
+                </Menu>
+                <Dots
+                  className={isShow ? "hidde" : "dots_tour_detail pointer"}
+                  onClick={handleClick}
+                />
+              </div>
+            </Accordion.Title>
+            <Accordion.Content className={!isShow ? "hidde" : "active"}>
+              <Menu secondary className="_tour_detail_nav mobile">
+                <div className="row_mob">
+                  <Menu.Item
+                    name="Overview"
+                    active={activeItem === "Overview"}
+                    onClick={handleItemClick}
+                  />
+                  <Menu.Item
+                    name="Itinerary"
+                    active={activeItem === "Itinerary"}
+                    onClick={handleItemClick}
+                  />
+                </div>
+                <div className="row_mob">
+                  <Menu.Item
+                    onClick={handleItemClick}
+                    name="Accomodation"
+                    active={activeItem === "Accomodation"}
+                  />
+                  <Menu.Item
+                    name="Gallery"
+                    active={activeItem === "Gallery"}
+                    onClick={handleItemClick}
+                  />
+                </div>
+                <div className="row_mob center">
+                  <Menu.Item
+                    name="FAQ and Reviews"
+                    active={activeItem === "FAQ & Reviews"}
+                    onClick={handleItemClick}
+                  />
+                </div>
+              </Menu>
+              <div className="flex" style={{ padding: ".5rem" }}>
+                <Icon
+                  onClick={handleClick}
+                  name="angle double up"
+                  size="big"
+                  className="pointer"
+                  style={{
+                    color: "white",
+                  }}
+                />
+              </div>
+            </Accordion.Content>
+          </Accordion>
+        </div>
       </div>
       {activeItem === "Overview" && <TourOverView />}
       {activeItem === "Itinerary" && <TourItinerery />}
